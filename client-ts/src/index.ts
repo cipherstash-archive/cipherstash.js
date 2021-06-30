18002250354094909b65d19e306e46f4bda5a42d
James Sadler <james@cipherstash.com>
2021-06-30 18:18:33 +1000

    Export the parts of the client that customers will use
    This will come in handy for the example code module too.

---
+++ client-ts/src/index.ts
@@ -0,0 +1,4 @@
+export { Collection } from "./collection"
+export { Session } from "./session"
+export { downcase, ngram, standardTokenizer } from "./dsl/filters-and-tokenizers-dsl";
+export { and } from './dsl/query-dsl'
