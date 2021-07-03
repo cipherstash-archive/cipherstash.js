4888313406e7515a6813a18af2fde66aa6d3b7dc
James Sadler <james@cipherstash.com>
2021-07-04 00:04:03 +1000

    Improve comment re: default key in siphash code

--- client-ts/src/crypto/sip-hash.ts
+++ client-ts/src/crypto/sip-hash.ts
@@ -1,6 +1,7 @@
 import * as SipHash from 'siphash'
 
-// FIXME: this should probably not be hardcoded.
+// Note that this key is not a cryptographic secret. All siphash hashes will be
+// ORE encrypted.  The plain hashes are never saved.
 export const DefaultKey: SipHash.Key = [ 0xdeadbeef, 0xcafebabe, 0x8badf00d, 0x1badb002 ];
 
 export type SipHashBuffer = { sipHash: Buffer }
@@ -8,7 +9,6 @@ export type SipHashBuffer = { sipHash: Buffer }
 export type MakeHashFn = (key: SipHash.Key) => (term: string) => SipHashBuffer
 
 export const makeHashFn: MakeHashFn = (key) => (term) => {
-  // FIXME: should we also prepend a salt to the string before hashing?
   const {h: h, l: l} = SipHash.hash(key, term)
   const buff = Buffer.alloc(8)
   buff.writeUInt32BE(h)
