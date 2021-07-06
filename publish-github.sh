d1c2cce0d64dd309bcca262a9541999e06156e1c
James Sadler <james@cipherstash.com>
2021-07-06 15:07:31 +1000

    Utility script to publish JS code to Github
    Uses git-subtree under the hood.

---
+++ publish-github.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script updates the public Github repositories the packages in this dir.
+
+set -e # exit when a command fails
+set -u # exit when script tries to use undeclared variables
+set -x # trace what gets executed (useful for debugging)
+
+platform_root=$(git rev-parse --show-toplevel)
+
+for package in stashjs-grpc stashjs stashjs-examples; do
+  remote=$(node -e "console.log(require('./$package/package.json').repository.url)" | sed 's/^git\+//')
+  echo "Remote: $remote"
+  (cd "$platform_root" && git subtree push --prefix="clients/@cipherstash/$package" "$remote" main)
+done
