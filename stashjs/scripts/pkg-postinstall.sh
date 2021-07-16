51ddfa41a4d3e47050d7fbecb4167e3b6849dade
James Sadler <james@cipherstash.com>
2021-07-16 11:19:35 +1000

    Fix some bugs with the package publishing process

--- stashjs/scripts/pkg-postinstall.sh
+++ stashjs/scripts/pkg-postinstall.sh
@@ -8,7 +8,6 @@ set -u # exit when script tries to use undeclared variables
 set -x # trace what gets executed (useful for debugging)
 
 if [ -d ./src/crypto/ore/fastore ]; then
-  rm -fr ./dist
   node-gyp configure
   node-gyp build
   cp src/crypto/ore/napi_ore.d.ts build/Release/
