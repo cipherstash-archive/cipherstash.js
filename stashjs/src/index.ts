7eb4e1e4169f0a5a0800984d3895b1435f45b361
James Sadler <james@cipherstash.com>
2021-07-26 17:10:11 +1000

    Export some more types and bump version

--- stashjs/src/index.ts
+++ stashjs/src/index.ts
@@ -1,6 +1,34 @@
 export { Stash } from "./stash"
 export { Collection } from "./collection"
 export { CollectionSchema } from "./collection-schema"
-export { StashRecord, Mappings, MappingsMeta, MappingsDSL, MappingOn } from "./dsl/mappings-dsl"
-export { all, Query, QueryBuilder } from './dsl/query-dsl'
-export { downcase, upcase, ngram, standard } from "./dsl/filters-and-tokenizers-dsl";
+export {
+  DynamicMatchMapping,
+  EncryptedIndexId,
+  ExactMapping,
+  ExactMappingFieldType,
+  FieldDynamicMatchMapping,
+  FieldTypeOfMapping,
+  MappableFieldType,
+  MappingOn,
+  Mappings,
+  MappingsDSL,
+  MappingsMeta,
+  MatchMapping,
+  MatchMappingFieldType,
+  MatchOptions,
+  NewStashRecord,
+  RangeMapping,
+  RangeMappingFieldType,
+  StashRecord,
+} from "./dsl/mappings-dsl"
+export {
+  all,
+  Query,
+  QueryBuilder
+} from './dsl/query-dsl'
+export {
+  downcase,
+  upcase,
+  ngram,
+  standard
+} from "./dsl/filters-and-tokenizers-dsl";
