58027e8ee96870062f6c14f62b80cd440952bf9c
James Sadler <james@cipherstash.com>
2021-07-26 17:02:42 +1000

    Export some types that should be public

--- stashjs/src/index.ts
+++ stashjs/src/index.ts
@@ -1,5 +1,6 @@
+export { Stash } from "./stash"
 export { Collection } from "./collection"
 export { CollectionSchema } from "./collection-schema"
-export { Stash } from "./stash"
+export { StashRecord, Mappings, MappingsMeta, MappingsDSL, MappingOn } from "./dsl/mappings-dsl"
+export { all, Query, QueryBuilder } from './dsl/query-dsl'
 export { downcase, upcase, ngram, standard } from "./dsl/filters-and-tokenizers-dsl";
-export { all } from './dsl/query-dsl'
