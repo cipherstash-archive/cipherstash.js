c3cbb67d125965f9a3f29729e43cb15092873892
James Sadler <james@cipherstash.com>
2021-07-31 15:07:49 +1000

    Remove the old client

--- client-js/index.js
+++
@@ -1,8 +0,0 @@
-
-const Stash = require('./stash')
-const Collection = require('./collection')
-const CipherSuite = require('./cipher')
-const Query = require('./query')
-const AuthToken = require('./auth_token')
-
-module.exports = { AuthToken, Stash, Collection, CipherSuite, Query }
