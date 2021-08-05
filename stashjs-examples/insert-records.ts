021c9c7c04846e258e3eb7cee2cbc9d6a51b09d3
Dan Draper <dan@coderdan.co>
2021-08-05 22:57:00 +1000

    Clean up the demo queries

--- stashjs-examples/insert-records.ts
+++ stashjs-examples/insert-records.ts
@@ -10,46 +10,23 @@ async function insertRecords() {
     console.log(`Collection "${movies.name}" loaded`)
 
     const data = fs.readFileSync('titles.tsv', 'utf8')
-    console.log(data)
 
     parse(data, {
       delimiter: "\t"
-    }, function(_err, records) {
-      records.forEach((movie: Array<string>) => {
-        console.log(movie[2])
-        movies.put({
-          title: movie[2]!,
-          year: parseInt(movie[5]!),
-          runningTime: parseInt(movie[7]!)
-        }).catch((err: any) => console.log('ERROR', err))
-      })
+    }, async function(_err, records) {
+      for (let i = 0; i < records.length; i++) {
+        let movie = records[i]
+        let [title, year, runningTime] = [movie[2]!, parseInt(movie[5]!), parseInt(movie[7]!)]
+        if (!isNaN(year) && !isNaN(runningTime)) {
+          console.log(title)
+          await movies.put({
+            title: movie[2]!,
+            year: parseInt(movie[5]!),
+            runningTime: parseInt(movie[7]!)
+          })
+        }
+      }
     })
-
-      /*    await employees.put({
-      name: "Ada Lovelace",
-      jobTitle: "Chief Executive Officer (CEO)",
-      dateOfBirth: new Date(1852, 11, 27),
-      email: "ada@security4u.example",
-      grossSalary: 250000n
-    })
-
-    await employees.put({
-      name: "Grace Hopper",
-      jobTitle: "Chief Science Officer (CSO)",
-      dateOfBirth: new Date(1906, 12, 9),
-      email: "grace@security4u.example",
-      grossSalary: 250000n
-    })
-
-    await employees.put({
-      name: "Joan Clark",
-      jobTitle: "Chief Information Security Officer (CISO)",
-      dateOfBirth: new Date(1917, 6, 24),
-      email: "joan@security4u.example",
-      grossSalary: 250000n
-    })
-
-    console.log("Inserted 3 records")*/
   } catch (err) {
     console.error(`Could not insert records into collection! Reason: ${JSON.stringify(err)}`)
   }
--- stashjs-examples/query-collection.ts
+++ stashjs-examples/query-collection.ts
@@ -1,87 +1,27 @@
 import { Stash } from "@cipherstash/stashjs"
 import { movieSchema, Movie } from "./example-schema";
 
+function displayResults(result: any) {
+  result.documents.forEach((movie: Movie) => {
+    console.log({
+      title: movie.title,
+      year: movie.year,
+      runningTime: movie.runningTime
+    })
+    // TODO: print count (and ideally query time)
+  })
+}
+
 async function queryCollection() {
   try {
     const stash = await Stash.connect(Stash.loadConfigFromEnv())
     const movies = await stash.loadCollection(movieSchema)
 
-      /*let queryResult = await movies.query(movie => movie.title.match("biz bon"))
-    queryResult.documents.forEach((movie: Movie) => {
-      console.log(movie)
-      // TODO: print count (and ideally query time)
-    })*/
+    let queryResult = await movies.query(movie => movie.title.match("BLIN amb"), { limit: 50 })
+    displayResults(queryResult)
 
-    // TODO: I think gte isn't working
-    let queryResult = await movies.query(movie => movie.year.gte(1980))
-    queryResult.documents.forEach((movie: Movie) => {
-      console.log(movie)
-    })
-
-    /*if (queryResult.documents.length == 1 && queryResult.documents[0]!.name == "Ada Lovelace") {
-      console.log("☑️  Successfully queried using an `Exact` mapping")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }
-
-    queryResult = await employees.query(employee =>
-      employee.dateOfBirth.between(
-        new Date(Date.parse("1852-11-27")),
-        new Date(Date.parse("1917-12-09"))
-      )
-    )
-
-    if (queryResult.documents.length == 3) {
-      console.log("☑️  Successfully queried using a `Range` mapping!")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }
-
-    queryResult = await employees.query(employee =>
-      employee.dateOfBirth.between(
-        new Date(Date.parse("1852-11-27")),
-        new Date(Date.parse("1917-12-09"))
-      ),
-      { limit: 1 }
-    )
-
-    if (queryResult.documents.length == 1) {
-      console.log("☑️  Successfully queried using a `Range` mapping with a `limit` option!")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }
-
-    queryResult = await employees.query(employee =>
-      employee.dateOfBirth.between(
-        new Date(Date.parse("1852-11-27")),
-        new Date(Date.parse("1917-12-09"))
-      ),
-      { aggregation: [{ ofIndex: "dateOfBirth", aggregate: "count" }]}
-    )
-
-    if (queryResult.aggregates.length == 1 &&
-        queryResult.aggregates[0]!.name == "count" &&
-        queryResult.aggregates[0]!.value == 3n) {
-      console.log("☑️  Successfully queried using a `Range` mapping with an `aggregate` query option!")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }
-
-    queryResult = await employees.query(employee => employee.allStringFields1.match("Grace"))
-
-    if (queryResult.documents.length == 1 && queryResult.documents[0]!.name == "Grace Hopper") {
-      console.log("☑️  Successfully queried using a `DynamicMatch` mapper!")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }
-
-    queryResult = await employees.query(employee => employee.allStringFields2.match("name", "hopper"))
-
-    if (queryResult.documents.length == 1 && queryResult.documents[0]!.name == "Grace Hopper") {
-      console.log("☑️  Successfully queried using a `FieldDynamicMatch` mapper!")
-    } else {
-      console.error(`Unexpected result: ${JSON.stringify(queryResult)}`)
-    }*/
+    queryResult = await movies.query(movie => movie.year.gte(2019), { limit: 50 })
+    displayResults(queryResult)
 
   } catch (err) {
     console.error(`Could not query collection! Reason: ${JSON.stringify(err)}`)
