d2d2068d9a4c2aa3ed8be1dcc640e747c557eef5
James Sadler <james@cipherstash.com>
2021-08-30 12:21:38 +1000

    Trap build.sh errors so failing is a little LOUDER

--- stashjs/scripts/pkg-build.sh
+++ stashjs/scripts/pkg-build.sh
@@ -7,6 +7,8 @@ set -e # exit when a command fails
 set -u # exit when script tries to use undeclared variables
 set -x # trace what gets executed (useful for debugging)
 
+trap "echo SOMETHING WENT WRONG - please read the logs above and see if it helps you figure out what's wrong - and also ask an engineer help :)" ERR
+
 rm -fr ./dist ./src/crypto/ore/fastore/*
 mkdir -p ./src/crypto/ore/fastore
 cp -R ../../../fastore/* ./src/crypto/ore/fastore
