393a5fabf4dfcb94bc0379325c9b5591f25a56b7
James Sadler <james@cipherstash.com>
2021-08-30 12:21:38 +1000

    Trap build.sh errors so failing is a little LOUDER

--- stashjs-grpc/scripts/pkg-build.sh
+++ stashjs-grpc/scripts/pkg-build.sh
@@ -7,11 +7,13 @@ set -e # exit when a command fails
 set -u # exit when script tries to use undeclared variables
 set -x # trace what gets executed (useful for debugging)
 
- rm -fr ./grpc ./dist ./generated
- mkdir -p ./grpc ./dist ./generated
- cp -R ../../../data-service/priv/grpc/* ./grpc
- proto-loader-gen-types api.proto --outDir=generated --grpcLib=@grpc/grpc-js \
-    --includeDirs grpc/v1 --keepCase=true --longs=number --enums=string \
-    --defaults=true --oneofs==true
+trap "echo SOMETHING WENT WRONG - please read the logs above and see if it helps you figure out what's wrong - and also ask an engineer help :)" ERR
+
+rm -fr ./grpc ./dist ./generated
+mkdir -p ./grpc ./dist ./generated
+cp -R ../../../data-service/priv/grpc/* ./grpc
+proto-loader-gen-types api.proto --outDir=generated --grpcLib=@grpc/grpc-js \
+  --includeDirs grpc/v1 --keepCase=true --longs=number --enums=string \
+  --defaults=true --oneofs==true
 pnpx tsc
 cp -RL grpc dist
