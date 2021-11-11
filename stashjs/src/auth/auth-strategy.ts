3bd9ee466e071e67fc74fdddd08e81ef508e7b76
James Sadler <james@cipherstash.com>
2021-11-11 12:08:16 +1100

    Add helper to cache expensive objects that depend on auth creds
    Building a CipherSuite is an expensive operation - it requires talking
    to KMS. So we want to avoid building them unnecessarily.
    
    To this end, I have added a new function `withFreshCredentials` that
    given an `AuthStrategy` and a callback that can build an object with
    fresh credentials, it will return a `Memo` that will cache the built
    value and rebuild it when ever it's asked for its object and the
    credentials have since expired (they will be refreshed, and a new object
    built).

--- stashjs/src/auth/auth-strategy.ts
+++ stashjs/src/auth/auth-strategy.ts
@@ -1,7 +1,10 @@
 import { AWSClientConfig } from '../aws'
 
+export type AuthenticationDetails =
+  {authToken: string, awsConfig: AWSClientConfig}
+
 export type AuthenticationDetailsCallback<R> =
-  (input: {authToken: string, awsConfig: AWSClientConfig}) => Promise<R>
+  (input: AuthenticationDetails) => Promise<R>
 
 export interface AuthStrategy {
   /**
@@ -14,18 +17,62 @@ export interface AuthStrategy {
   initialise(): Promise<void>
 
   /**
-   * Wraps a request to a downstream service with authentication.
+   * Executes a callback and provides the callback with authentication details.
    *
-   * The callback will be invoked in which * the call to the downstream service
-   * can be made with the token.
+   * This can be used to wrap a request to a downstream service that requires
+   * the credentials.
    *
-   * If a cached token has expired a token refresh will be performed before
-   * execution of the callback.
+   * If the authentication token has expired this method will refresh the token
+   * before invoking the callback.
    *
    * Returns the result of the callback.
    *
    * If an authentication attempt fails, or an attempt to refresh the token
    * fails then the Promise will be rejected with an error.
    */
-  authenticatedRequest<R>(callback: AuthenticationDetailsCallback<R>): Promise<R>
+  withAuthentication<R>(callback: AuthenticationDetailsCallback<R>): Promise<R>
+
+  /**
+   * Returns true if the credentials managed by this AuthStrategy have not expired.
+   * If there are multiple sets of credentials (i.e. Auth0 access token + federated
+   * AWS credentials) then this must return false if *any* of them have expired.
+   *
+   * If the authentication state name is anything other than "authenticated" then this
+   * method must return false.
+   */
+  isFresh(): boolean
+}
+
+/**
+ * Use this function to obtain a Memo that will build a value of type T & cache
+ * it. The value depends on having authentication credentials that have not
+ * expired - when those credentials expire, the `freshValue` function will
+ * trigger off a refresh of the authentication token.
+ *
+ * Use a Memo when you need to build an object that depends on having fresh,
+ * unexpired credentials.
+ *
+ * A reference to the returned Memo object can be kept indefinitely.
+ *
+ * @param authStrategy - your AuthStrategy instance
+ * @param builder - a function that when provided with up to date authentication
+ *                  credentials will build a new value
+ * @returns a Memo<T>
+ */
+export function withFreshCredentials<T>(authStrategy: AuthStrategy, builder: AuthenticationDetailsCallback<T>): Memo<T> {
+  let latestValuePromise = authStrategy.withAuthentication(builder)
+  return {
+    async freshValue() {
+      if (authStrategy.isFresh()) {
+        return latestValuePromise
+      } else {
+        latestValuePromise = authStrategy.withAuthentication(builder)
+        return latestValuePromise
+      }
+    }
+  }
+}
+
+export interface Memo<T> {
+  freshValue(): Promise<T>
 }
