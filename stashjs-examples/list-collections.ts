549b5176e4321ac1a81c795df007c283d535bf54
James Sadler <james@cipherstash.com>
2021-11-16 15:37:57 +1100

    Implement command and example to list collections + metadata

--- stashjs-examples/list-collections.ts
+++ stashjs-examples/list-collections.ts
@@ -4,7 +4,13 @@ async function listCollections() {
   try {
     const stash = await Stash.connect()
     const collectionNames = await stash.listCollections()
-    console.log(`Collection names: ${collectionNames.join(", ")}`)
+    collectionNames.forEach(async name => {
+      const collection = await stash.loadCollection(name)
+      console.log(`Collection: "${name}`)
+      Object.entries(collection.schema.mappings).forEach(async ([mapping, settings]) => {
+        console.log(`\t${mapping}`)
+      })
+    })
   } catch (err) {
     console.error(`Could not list collections! Reason: ${describeError(err)}`)
     console.error(err)
