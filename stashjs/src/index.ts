6dbdefe15be759ed5927f1b82eb232ed447edbfb
James Sadler <james@cipherstash.com>
2021-12-06 14:34:51 +1100

    StashJS: export the auth strategies for StashCLI

--- stashjs/src/index.ts
+++ stashjs/src/index.ts
@@ -105,3 +105,9 @@ export {
 export * as errors from './errors'
 
 export * from './result'
+
+export { AuthStrategy } from './auth/auth-strategy'
+export { makeAuthStrategy } from './auth/make-auth-strategy'
+export { Auth0DeviceCodeStrategy } from './auth/auth0-device-code-strategy'
+export { Auth0AccessTokenStrategy } from './auth/auth0-access-token-strategy'
+export { Auth0Machine2MachineStrategy } from './auth/auth0-machine-2-machine-strategy'
