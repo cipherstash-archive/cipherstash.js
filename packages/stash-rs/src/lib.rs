fdc82d93f3c3802888d067589eeac53c8c6c683c
Dan Draper <dan@coderdan.co>
2021-12-16 21:39:43 +1100

    Added ability to pass keys to init function

--- packages/stash-rs/src/lib.rs
+++ packages/stash-rs/src/lib.rs
@@ -1,6 +1,6 @@
-use neon::prelude::*;
-use ore_rs::{PlainText, ORECipher, OREError, scheme::bit2::OREAES128};
 use hex_literal::hex;
+use neon::prelude::*;
+use ore_rs::{scheme::bit2::OREAES128, ORECipher, OREError, PlainText};
 use std::cell::RefCell;
 
 struct Cipher(OREAES128);
@@ -14,10 +14,23 @@ type BoxedCipher = JsBox<RefCell<Cipher>>;
 // and implement the OREEncrypt trait for a JsBuffer
 
 fn init(mut cx: FunctionContext) -> JsResult<BoxedCipher> {
-    let k1: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
-    let k2: [u8; 16] = hex!("00010203 04050607 08090a0b 0c0d0e0f");
+    let arg0 = cx.argument::<JsBuffer>(0)?;
+    let arg1 = cx.argument::<JsBuffer>(1)?;
     let seed = hex!("00010203 04050607");
-    let cipher: OREAES128 = ORECipher::init(k1, k2, &seed).or_else(|e| cx.throw_error("Could not init cipher"))?;
+
+    let clone_key = |data: neon::borrow::Ref<'_, BinaryData<'_>>| {
+        let mut k: [u8; 16] = Default::default();
+        let slice = data.as_slice::<u8>();
+        if slice.len() != 16 { return Err("Invalid key length") }
+        k.clone_from_slice(data.as_slice::<u8>());
+        return Ok(k);
+    };
+
+    let k1 = cx.borrow(&arg0, clone_key).or_else(|e| cx.throw_error(e))?;
+    let k2 = cx.borrow(&arg1, clone_key).or_else(|e| cx.throw_error(e))?;
+
+    let cipher: OREAES128 =
+        ORECipher::init(k1, k2, &seed).or_else(|_e| cx.throw_error("Could not init cipher"))?;
 
     let ore = RefCell::new(Cipher(cipher));
 
@@ -26,22 +39,22 @@ fn init(mut cx: FunctionContext) -> JsResult<BoxedCipher> {
 
 fn encrypt(mut cx: FunctionContext) -> JsResult<JsBuffer> {
     let cipher = cx.argument::<BoxedCipher>(0)?;
-    let mut ore = &mut *cipher.borrow_mut();
+    let ore = &mut *cipher.borrow_mut();
     let input = cx.argument::<JsBuffer>(1)?;
-    
-    let x = cx.borrow(&input, |data| {
-        let mut plaintext: PlainText<8> = Default::default();
-        let slice = data.as_slice::<u8>();
-        /*if slice.len() != 8 {
-            return Err("Input too short");
-        }*/
-        /* TODO: ORE encrypt should just take a slice of u8 and we can avoid this clone. */
-        plaintext.clone_from_slice(slice);
-        // TODO: Should we encrypt _inside_ the borrow block or later? What about a promise?
-        Ok(ore.0.encrypt(&plaintext)?.to_bytes())
-    }).or_else(|err: OREError| {
-        cx.throw_error("ORE error")
-    })?;
+
+    let x = cx
+        .borrow(&input, |data| {
+            let mut plaintext: PlainText<8> = Default::default();
+            let slice = data.as_slice::<u8>();
+            /*if slice.len() != 8 {
+                return Err("Input too short");
+            }*/
+            /* TODO: ORE encrypt should just take a slice of u8 and we can avoid this clone. */
+            plaintext.clone_from_slice(slice);
+            // TODO: Should we encrypt _inside_ the borrow block or later? What about a promise?
+            Ok(ore.0.encrypt(&plaintext)?.to_bytes())
+        })
+        .or_else(|_: OREError| cx.throw_error("ORE error"))?;
 
     // Looking at the source code, this appears to do an unsafe memory reinterpret (so it is
     // probably fast)
