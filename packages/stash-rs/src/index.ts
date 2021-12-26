979030e0ea0cd3b673f2f02292730b0022253073
Dan Draper <dan@coderdan.co>
2021-12-26 16:31:10 +1100

    Added compare function and fleshed out basic tests

--- packages/stash-rs/src/index.ts
+++ packages/stash-rs/src/index.ts
@@ -1,15 +1,21 @@
 "use strict";
 
 /* Importing the rust compiled lib (index.node) doesn't work and so we use require here */
-const { initCipher, encrypt } = require("../index.node");
+const {
+  initCipher,
+  encrypt_buf,
+  encrypt_num,
+  compare
+} = require("../index.node");
 
 export type Key = Buffer;
-export type Plaintext = Buffer;
+export type PlainText = number | bigint
 export type CipherText = Buffer;
 
 export type ORECipher = {
-  encrypt: (input: Plaintext) => CipherText
+  encrypt: (input: PlainText) => CipherText
 }
+
 
 // TODO: make encrypt return a promise using promisify?
 // TODO: Add tests!
@@ -17,10 +23,21 @@ export const ORE = {
   init: (k1: Key, k2: Key): ORECipher => {
     let cipher = initCipher(k1, k2);
     return {
-      encrypt: (buf) => {
-        return encrypt(cipher, buf)
+      encrypt: (input: PlainText) => {
+        if (typeof input === 'bigint') {
+          // Neon doesn't support Bigint so we do this here
+          let buf = Buffer.allocUnsafe(8);
+          buf.writeBigInt64BE(input);
+          return encrypt_buf(cipher, buf);
+        } else {
+          return encrypt_num(cipher, input);
+        }
       }
     }
+  },
+
+  compare: (a: CipherText, b: CipherText): any => { // TODO: Enum integer type
+    return compare(a, b);
   }
 };
 
