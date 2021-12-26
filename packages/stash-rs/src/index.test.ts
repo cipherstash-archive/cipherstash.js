cb3f61135d3b6c73f16b2bb518ccac7041fa1970
Dan Draper <dan@coderdan.co>
2021-12-26 16:31:10 +1100

    Added compare function and fleshed out basic tests

--- packages/stash-rs/src/index.test.ts
+++ packages/stash-rs/src/index.test.ts
@@ -6,8 +6,11 @@ describe("ORE", () => {
     let k2 = Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
 
     let ore = ORE.init(k1, k2);
-    let plaintext = Buffer.allocUnsafe(8);
-    plaintext.writeBigInt64BE(456n);
-    ore.encrypt(plaintext);
+
+    let ct_a = ore.encrypt(456n);
+
+    let ct_b = ore.encrypt(100n);
+
+    expect(ORE.compare(ct_a, ct_b)).toEqual(1);
   })
 })
