bd4e34d09618019edd9cace6bbaa6fb8d59bb581
Dan Draper <dan@coderdan.co>
2021-12-29 17:06:35 +1100

    Copied over docs from C code

--- packages/stash-rs/native/convert.rs
+++ packages/stash-rs/native/convert.rs
@@ -1,3 +1,27 @@
+/*
+  This following function implement an order-preserving translation of 64 bit
+  floats to 64 bit doubles (and the reverse operation - although that is just
+  used for verifying correctness).
+
+  The 64 bit integer that is produced is a plaintext that will be ORE encrypted
+  later on.
+  
+  The mapping is such that the ordering of the floats will be preserved when
+  mapped to an unsigned integer, for example, an array of unsigned integers
+  dervived from a sorted array of doubles will result in no change to its
+  ordering when it itself is sorted.
+
+  The mapping does not preserve any notion of the previous value after the
+  conversion - only ordering is preserved.
+  
+  Caveat: NaN and -ve & +ve infinity will also be mapped and ordering is not
+  well-defined with those values. Those values should be discarded before
+  converting arrays of those values.
+
+  This post was used as a reference for building this implementation:
+  https://lemire.me/blog/2020/12/14/converting-floating-point-numbers-to-integers-while-preserving-order
+*/
+
 use core::mem;
 
 pub(crate) trait ToOrderedInteger<T> {
