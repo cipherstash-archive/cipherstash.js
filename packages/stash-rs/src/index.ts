39fdcc714ded7e50b93532acd70fda261d5e59b1
Dan Draper <dan@coderdan.co>
2022-01-04 11:41:39 +1100

    Updated README and added docs

--- packages/stash-rs/src/index.ts
+++ packages/stash-rs/src/index.ts
@@ -20,10 +20,16 @@ export type ORECipher = {
 }
 
 // TODO: make encrypt return a promise using promisify?
+/* Module to perform Order-revealing Encryption using the underlying ore.rs Rust library */
 export const ORE = {
+  /* Initialize a new ORE cipher with a key pair (both keys must be 16-byte buffers). */
   init: (k1: Key, k2: Key): ORECipher => {
     let cipher = initCipher(k1, k2);
     return {
+      /* 
+       * Encrypt the given `PlainText` outputting a "full" CipherText (i.e. a `Buffer`
+       * containing both the Left and Right components).
+       */
       encrypt: (input: PlainText): CipherText => {
         if (typeof input === 'bigint') {
           // Neon doesn't support Bigint so we do this here
@@ -37,6 +43,10 @@ export const ORE = {
         }
       },
 
+      /* 
+       * Encrypt the given `PlainText` outputting only a Left CipherText (i.e. a `Buffer`
+       * containing just the Left component).
+       */
       encryptLeft: (input: PlainText): CipherText => {
         if (typeof input === 'bigint') {
           // Neon doesn't support Bigint so we do this here
@@ -52,6 +62,10 @@ export const ORE = {
     }
   },
 
+  /*
+   * Compare two cipher texts returning -1 if a < b, 0 if a === b, and 1 if a > b.
+   * Throws if the inputs are not comparable.
+   */
   compare: (a: CipherText, b: CipherText): any => { // TODO: Enum integer type
     return compare(a, b);
   }
