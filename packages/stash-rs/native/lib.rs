9bf9838c7538a120e7911aa6545ded21443db70a
James Sadler <james@cipherstash.com>
2022-02-02 09:26:07 +1100

    Export ORE range creation utils

--- packages/stash-rs/native/lib.rs
+++ packages/stash-rs/native/lib.rs
@@ -1,5 +1,11 @@
 use hex_literal::hex;
 use neon::{prelude::*};
+use ore_encoding_rs::OreRange;
+use ore_encoding_rs::encode_between;
+use ore_encoding_rs::encode_gt;
+use ore_encoding_rs::encode_gte;
+use ore_encoding_rs::{encode_lt, encode_lte};
+use ore_encoding_rs::encode_eq;
 use ore_rs::{scheme::bit2::OREAES128, ORECipher, OREEncrypt};
 use ore_encoding_rs::OrePlaintext;
 use ore_encoding_rs::siphash;
@@ -123,14 +129,72 @@ fn encode_buffer(mut cx: FunctionContext) -> JsResult<JsNumber> {
     }
 }
 
+fn make_range_object(mut cx: FunctionContext, min: OrePlaintext<u64>, max: OrePlaintext<u64>) -> JsResult<JsObject> {
+    let obj = cx.empty_object();
+    let js_min = cx.number(f64::from_bits(min.0));
+    let js_max = cx.number(f64::from_bits(max.0));
+    obj.set(&mut cx, "min", js_min)?;
+    obj.set(&mut cx, "max", js_max)?;
+    Ok(obj)
+}
+
+fn encode_range_lt(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_lt(OrePlaintext(value.value(&mut cx).to_bits()));
+    make_range_object(cx, min, max)
+}
+
+fn encode_range_lte(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_lte(OrePlaintext(value.value(&mut cx).to_bits()));
+    make_range_object(cx, min, max)
+}
+
+fn encode_range_gt(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_gt(OrePlaintext(value.value(&mut cx).to_bits()));
+    make_range_object(cx, min, max)
+}
+
+fn encode_range_gte(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_gte(OrePlaintext(value.value(&mut cx).to_bits()));
+    make_range_object(cx, min, max)
+}
+
+fn encode_range_eq(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_eq( OrePlaintext(value.value(&mut cx).to_bits()));
+    make_range_object(cx, min, max)
+}
+
+fn encode_range_between(mut cx: FunctionContext) -> JsResult<JsObject> {
+    let value1 = cx.argument::<JsNumber>(0)?;
+    let value2 = cx.argument::<JsNumber>(0)?;
+    let OreRange{ min, max } = encode_between(
+        OrePlaintext(value1.value(&mut cx).to_bits()),
+        OrePlaintext(value2.value(&mut cx).to_bits()),
+    );
+    make_range_object(cx, min, max)
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("encodeNumber", encode_num)?;
     cx.export_function("encodeString", encode_string)?;
     cx.export_function("encodeBuffer", encode_buffer)?;
+
+    cx.export_function("encodeRangeEq", encode_range_eq)?;
+    cx.export_function("encodeRangeGt", encode_range_gt)?;
+    cx.export_function("encodeRangeGte", encode_range_gte)?;
+    cx.export_function("encodeRangeLt", encode_range_lt)?;
+    cx.export_function("encodeRangeLte", encode_range_lte)?;
+    cx.export_function("encodeRangeBetween", encode_range_between)?;
+
+    cx.export_function("initCipher", init)?;
     cx.export_function("encrypt", encrypt_num)?;
     cx.export_function("encryptLeft", encrypt_num_left)?;
-    cx.export_function("initCipher", init)?;
+
     cx.export_function("compare", compare)?;
     Ok(())
 }
