4f43dc08afcb8f17dd6fb95376e063ae203316f7
James Sadler <james@cipherstash.com>
2022-02-23 16:24:38 +1100

    Add --help option to create-collection command

--- stash-cli/src/commands/create-collection.ts
+++ stash-cli/src/commands/create-collection.ts
@@ -24,6 +24,16 @@ const command: GluegunCommand = {
     const { print, parameters } = toolbox
     const options = parameters.options
 
+    if (options.help) {
+      print.info("Usage: stash create-collection [--profile <profile>] [--schema <schema>] [--help]")
+      print.info("")
+      print.info("Creates a collection in the workspace of the profile\n")
+      print.info("See also https://docs.cipherstash.com/reference/stash-cli/stash-create-collection.html")
+      print.info("")
+      process.exit(1)
+      return
+    }
+
     if (!parameters.first) {
       print.error(`expected parameter --name`)
       process.exit(1)
