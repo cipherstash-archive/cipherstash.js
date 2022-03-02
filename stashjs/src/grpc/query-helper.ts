c315af3b16679213a82135891e32527c76a15aea
Matt Palmer <mpalmer@hezmatt.org>
2022-03-02 19:51:09 +1100

    Use monotonic timers everywhere
    Saves so much grief when clocks slew and DST changes (although the latter
    should never happen on servers, because we're not idiots)

--- stashjs/src/grpc/query-helper.ts
+++ stashjs/src/grpc/query-helper.ts
@@ -4,16 +4,17 @@ import { CipherSuite } from "../crypto/cipher";
 import { HasID, StashRecord } from "../dsl/mappings-dsl"
 import { DecryptionFailure } from "../errors";
 import { AsyncResult, Err, gatherAsync, Ok } from "../result"
+import { durationSeconds } from "../utils"
 
 export async function convertQueryReplyToQueryResult<R extends StashRecord & HasID>(
   cipher: CipherSuite,
-  timerStart: number,
+  timerStart: bigint,
   reply: V1.Query.QueryReply
 ): AsyncResult<QueryResult<R & HasID>, DecryptionFailure> {
   const records = await gatherAsync(reply.result!.map(encryptedSource => cipher.decrypt<R>(encryptedSource)))
   if (records.ok) {
     return Ok({
-      took: (new Date().getTime() - timerStart) / 1000,
+      took: durationSeconds(timerStart, process.hrtime.bigint()),
       documents: records.value,
       aggregates: reply!.aggregates ? reply!.aggregates.map(agg => ({
         name: agg.name! as Aggregate,
