049ad3f1e14bd5311ddf1e652d955159aea5c164
James Sadler <james@cipherstash.com>
2022-03-02 15:49:58 +1100

    Fix: MaxListenersExceededWarning: Possible EventEmitter memory leak detected
    This error is sometimes seen on large imports.
    
    The StreamWriter class used NodeJS's EventEmitter API to detect "error"
    and "drain" events on streams. In particular it uses the "once" method
    to handle an event one time which then removes the listener for you once
    it has been fired.
    
    The problem was that we are listening for not one but *two* events and
    they were mutually exclusive - so every time a stream write was buffered
    it would wait for one of the two events, one of which by definition
    would never happen so the listeners for one of the events (the error
    event in particular) were never freed.

--- stashjs/src/stream-writer.ts
+++ stashjs/src/stream-writer.ts
@@ -81,33 +81,48 @@ export class StreamWriter<
     }
   }
 
-
-  private async writeStreamingPutBegin(stream: ClientWritableStream<V1.Document.StreamingPutRequest>, collectionId: Uint8Array): AsyncResult<void, StreamingPutFailure> {
-    const promise = new Promise<void>(async (resolve, reject) => {
-      if (!stream.write({ begin: { collectionId }}, (err: any) => reject(err))) {
-        stream.once('error', (err: any) => reject(err))
-        stream.once('drain', () => resolve(void 0))
-      } else {
-        process.nextTick(() => resolve(void 0))
-      }
+  private writeStreamingPutBegin(stream: ClientWritableStream<V1.Document.StreamingPutRequest>, collectionId: Uint8Array): AsyncResult<void, StreamingPutFailure> {
+    const promise = new Promise<void>((resolve, reject) => {
+      this.writeWithDrainAndErrorHandlers(stream, { begin: { collectionId }}, resolve, reject)
     })
 
     return fromPromise(promise, (err: any) => err)
   }
 
-  private async writeOneStreamingPutRequest(stream: ClientWritableStream<V1.Document.StreamingPutRequest>, analysisResult: AnalysisResult): AsyncResult<void, StreamingPutFailure> {
+  private writeOneStreamingPutRequest(stream: ClientWritableStream<V1.Document.StreamingPutRequest>, analysisResult: AnalysisResult): AsyncResult<void, StreamingPutFailure> {
     let payload = this.toStreamingPutRequest(analysisResult)
     payload.document!.source = maybeGenerateId(payload.document?.source)
-    const promise = new Promise<void>(async (resolve, reject) => {
-      if (!stream.write(payload, (err: any) => reject(err))) {
-        stream.once('error', (err: any) => reject(err))
-        stream.once('drain', () => resolve(void 0))
-      } else {
-        process.nextTick(() => resolve(void 0))
-      }
+    const promise = new Promise<void>((resolve, reject) => {
+      this.writeWithDrainAndErrorHandlers(stream, payload, resolve, reject)
     })
 
     return fromPromise(promise, (err: any) => err)
+  }
+
+  private writeWithDrainAndErrorHandlers<Payload>(
+    stream: ClientWritableStream<Payload>,
+    payload: Payload,
+    resolve: (value: void | PromiseLike<void>) => void ,
+    reject: (reason?: any) => void
+  ): void {
+      const cleanupListeners = () => {
+        stream.removeListener('error', errorListener)
+        stream.removeListener('drain', drainListener)
+      }
+      const errorListener = (err: any) => {
+        cleanupListeners()
+        reject(err)
+      }
+      const drainListener = () => {
+        cleanupListeners()
+        resolve(void 0)
+      }
+      if (!stream.write(payload, (err: any) => { cleanupListeners(); reject(err) })) {
+        stream.once('error', errorListener)
+        stream.once('drain', drainListener)
+      } else {
+        process.nextTick(() => resolve(void 0))
+      }
   }
 }
 
