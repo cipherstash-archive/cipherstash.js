d3e3e1fb05cacc3246ab085add7946b373b1b124
Bennett Hardwick <bennett@cipherstash.com>
2022-03-17 12:06:25 +1000

    Improve `stash create-collection` usability
    - Add no-schema to explicitly not use a schema
    - Add checks for multiple params to catch quote mistakes
    - Remove dead `return` statements
    - Add some examples in error messages

--- stash-cli/src/commands/create-collection.ts
+++ stash-cli/src/commands/create-collection.ts
@@ -13,9 +13,17 @@ import {
   StashRecord,
   Result,
   Err,
-  Ok,
+  Ok
 } from '@cipherstash/stashjs'
 import { parseCollectionSchemaJSON } from '@cipherstash/stashjs/dist/parsers/collection-schema-parser'
+
+function formatCollectionName(name: string): string {
+  if (name.includes(' ')) {
+    return `"${name}"`
+  } else {
+    return name
+  }
+}
 
 const command: GluegunCommand = {
   name: 'create-collection',
@@ -24,18 +32,36 @@ const command: GluegunCommand = {
     const { print, parameters } = toolbox
     const options = parameters.options
 
-    if (options.help) {
-      print.info('Usage: stash create-collection <collection-name> [--profile <profile>] [--schema <schema>] [--help]')
+    function exitWithUsage(): never {
+      print.info(
+        'Usage: stash create-collection <collection-name> [--profile <profile>] [--schema <schema>] [--no-schema] [--help]'
+      )
       print.info('')
       print.info('Creates a collection in the workspace of the profile\n')
       print.info('See also https://docs.cipherstash.com/reference/stash-cli/stash-create-collection.html')
       print.info('')
       process.exit(0)
-      return
     }
 
-    if (!parameters.first) {
-      print.error(`expected collection name`)
+    if (options.help) {
+      exitWithUsage()
+    }
+
+    if (!parameters.array) {
+      exitWithUsage()
+    }
+
+    const [collectionName, ...unexpectedParameters] = parameters.array
+
+    if (unexpectedParameters.length > 0) {
+      print.error(`Recieved more than one value for collection name. Did you mean to use quotes?`)
+      print.error(`Example: stash create-collection "${collectionName} ${unexpectedParameters.join(' ')}"`)
+      process.exit(1)
+    }
+
+    if (!collectionName) {
+      print.error(`Expected a collection name`)
+      print.error(`Example: stash create-collection movies`)
       process.exit(1)
     }
 
@@ -52,30 +78,38 @@ const command: GluegunCommand = {
       if (!profile.ok) {
         print.error(`Could not load profile. Reason: "${describeError(profile.error)}"`)
         process.exit(1)
-        return
       }
     } catch (error) {
       print.error(`Unexpected error while loading profile. Reason: "${describeError(error)}"`)
       process.exit(1)
-      return
     }
 
     const connection = await StashInternal.connect(profile.value)
     if (!connection.ok) {
       print.error(`Authentication failed - please try to login again with "stash login"`)
       process.exit(1)
-      return
     }
     const stash = connection.value
-    const collectionName = parameters.first
 
-    let schema = await (options.schema
-      ? buildCollectionSchema(collectionName, options.schema)
-      : Ok.Async(CollectionSchema.define(collectionName).notIndexed()))
+    let schema: Result<CollectionSchema<StashRecord, Mappings<StashRecord>, any>, string>
+
+    // The flag --no-schema is parsed as { schema: false } by gluegun - so explicity check for this
+    if (options.schema === false) {
+      schema = Ok(CollectionSchema.define(collectionName).notIndexed())
+    } else if (options.schema) {
+      schema = buildCollectionSchema(collectionName, options.schema)
+    } else {
+      print.error(`Expected either --schema or --no-schema flags.`)
+      print.error('')
+      print.error(`If you meant to create a collection without a schema, try passing the --no-schema flag.`)
+      print.error(`Example: stash create-collection ${formatCollectionName(collectionName)} --no-schema`)
+      process.exit(1)
+    }
+
     if (schema.ok) {
       const created = await stash.createCollection(schema.value)
       if (created.ok) {
-        print.highlight(`The ${collectionName} collection has been created`)
+        print.highlight(`The ${formatCollectionName(collectionName)} collection has been created`)
         process.exit(0)
       } else {
         print.error(`Failed to create collection: ${JSON.stringify(created.error)}`)
