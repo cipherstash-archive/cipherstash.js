1f3e6b254e48298b141540049720b16ef31569e7
James Sadler <james@cipherstash.com>
2022-03-18 11:38:36 +1100

    StashJS: fix and improve error rendering
    Some bad types in the error consructors (i.e. when `cause` was of type
    `unknown`) meant some type errors were papered over.
    
    The overly "smart" wrap function was not handling all cases which meant
    that we didn't get proper stack traces printed for native javascript
    errors.
    
    The error ouput now looks like this:
    
    ```
    [CollectionListFailure] Failed to list collections
      └ [GRPCError] GRPC Error
          └ [JSError: Error] (14 UNAVAILABLE: No connection established)
              └ Error: 14 UNAVAILABLE: No connection established
              └     at Object.callErrorFromStatus (/Users/jamessadler/cipherstash/platform/clients/node_modules/.pnpm/@grpc+grpc-js@1.5.5/node_modules/@grpc/grpc-js/src/call.ts:81:24)
              └     at Object.onReceiveStatus (/Users/jamessadler/cipherstash/platform/clients/node_modules/.pnpm/@grpc+grpc-js@1.5.5/node_modules/@grpc/grpc-js/src/client.ts:343:36)
              └     at Object.onReceiveStatus (/Users/jamessadler/cipherstash/platform/clients/node_modules/.pnpm/@grpc+grpc-js@1.5.5/node_modules/@grpc/grpc-js/src/client-interceptors.ts:462:34)
              └     at Object.onReceiveStatus (/Users/jamessadler/cipherstash/platform/clients/node_modules/.pnpm/@grpc+grpc-js@1.5.5/node_modules/@grpc/grpc-js/src/client-interceptors.ts:424:48)
              └     at /Users/jamessadler/cipherstash/platform/clients/node_modules/.pnpm/@grpc+grpc-js@1.5.5/node_modules/@grpc/grpc-js/src/call-stream.ts:323:24
              └     at processTicksAndRejections (node:internal/process/task_queues:78:11)
    ```

--- stashjs/src/errors.ts
+++ stashjs/src/errors.ts
@@ -1,3 +1,4 @@
+import { unreachable } from "./type-utils"
 
 /**
  * An enumeration of all errors that can be thrown by CipherStash.
@@ -31,8 +32,10 @@ export type ErrorTag =
   | 'MalformedConfigFile'
   | 'MissingConfigDir'
   | 'MissingProfile'
+  | 'NativeError'
   | 'NoDefaultProfileSet'
   | 'OAuthFailure'
+  | 'PlainError'
   | 'SaveProfileFailure'
   | 'SetDefaultProfileFailure'
   | 'StreamingPutFailure'
@@ -40,8 +43,8 @@ export type ErrorTag =
 export type JSError = InstanceType<ErrorConstructor>
 
 export type NativeError = {
-  tag: 'NativeError'
-  cause: JSError
+  readonly tag: 'NativeError'
+  readonly cause: JSError
 }
 
 /**
@@ -52,14 +55,21 @@ export type NativeError = {
  * compiler can use to perform exhaustiveness checks should the handler of the
  * error want to alter behaviour based on the underlying cause.
  */
-export type StashJSError<Tag extends ErrorTag, Cause = undefined> = {
-  readonly tag: Tag
-  readonly message?: string
-  readonly cause?: Cause
-}
+export type StashJSError<Tag extends ErrorTag, Cause = undefined>
+  = Tag extends 'PlainError' ? {
+    readonly tag: Tag
+    readonly message: string
+  } : Tag extends 'NativeError' ? {
+    readonly tag: Tag
+    readonly cause: JSError
+  } : {
+    readonly tag: Tag
+    readonly message?: string
+    readonly cause?: Cause
+  }
 
-export type GRPCError = StashJSError<'GRPCError', NativeError>
-export const GRPCError: (cause: unknown) => GRPCError = (cause) => ({ tag: 'GRPCError', cause: wrap(cause) })
+export type GRPCError = StashJSError<'GRPCError', NativeError | AuthenticationFailure>
+export const GRPCError: (cause: GRPCError["cause"]) => GRPCError = (cause) => ({ tag: 'GRPCError', cause: wrap(cause) })
 
 export type AnalysisFailure = StashJSError<'AnalysisFailure', EncryptionFailure | AuthenticationFailure>
 export const AnalysisFailure: (cause: AnalysisFailure["cause"]) => AnalysisFailure = (cause) => ({ tag: 'AnalysisFailure', cause })
@@ -104,8 +114,8 @@ export const MissingProfile: (profileName: string) => MissingProfile = (profileN
 export type MalformedConfigFile = StashJSError<'MalformedConfigFile'>
 export const MalformedConfigFile: (profileName: string) => MalformedConfigFile = (fileName) => ({ tag: 'MalformedConfigFile', message: `Could not parse config file "${fileName}"` })
 
-export type IOError = StashJSError<'IOError', NativeError>
-export const IOError: (cause: unknown) => IOError = (cause) => ({ tag: 'IOError', cause: wrap(cause) })
+export type IOError = StashJSError<'IOError', NativeError | PlainError >
+export const IOError: (cause: IOError["cause"]) => IOError = (cause) => ({ tag: 'IOError', cause: wrap(cause) })
 
 export type LoadProfileFailure = StashJSError<'LoadProfileFailure', MissingConfigDir | NoDefaultProfileSet | MissingProfile | MalformedConfigFile | IOError>
 export const LoadProfileFailure: (cause: LoadProfileFailure["cause"]) => LoadProfileFailure = (cause) => ({ tag: 'LoadProfileFailure', cause })
@@ -122,14 +132,17 @@ export const SetDefaultProfileFailure: (cause: SetDefaultProfileFailure["cause"]
 export type SaveProfileFailure = StashJSError<'SaveProfileFailure', SetDefaultProfileFailure | MissingConfigDir | IOError>
 export const SaveProfileFailure: (cause: SaveProfileFailure["cause"]) => SaveProfileFailure = (cause) => ({ tag: 'SaveProfileFailure', cause })
 
-export type OAuthFailure = StashJSError<'OAuthFailure', NativeError>
-export const OAuthFailure: (cause: unknown) => OAuthFailure = (cause) => {
-  if (!!cause && (cause as any)['response']) {
-    return ({ tag: 'OAuthFailure', cause: wrap((cause as any).response) })
-  } else {
+export type OAuthFailure = StashJSError<'OAuthFailure', NativeError | PlainError>
+export const OAuthFailure: (cause: OAuthFailure["cause"], message?: string) => OAuthFailure = (cause, message) => {
+  if (typeof message === 'undefined') {
     return ({ tag: 'OAuthFailure', cause: wrap(cause) })
+  } else {
+    return ({ tag: 'OAuthFailure', message, cause: wrap(cause) })
   }
 }
+
+export type PlainError = StashJSError<'PlainError', undefined>
+export const PlainError: (message: string) => PlainError = message => ({ tag: 'PlainError', message })
 
 export type IllegalStateError = StashJSError<'IllegalStateError'>
 export const IllegalStateError: (message: string) => IllegalStateError = (message) => ({ tag: 'IllegalStateError', message })
@@ -140,16 +153,16 @@ export const AuthenticationFailure: (cause: AuthenticationFailure["cause"], mess
 export type ConnectionFailure = StashJSError<'ConnectionFailure', LoadProfileFailure | AuthenticationFailure | KMSError>
 export const ConnectionFailure: (cause: ConnectionFailure['cause']) => ConnectionFailure = (cause) => ({ tag: 'ConnectionFailure', cause })
 
-export type KMSError = StashJSError<'KMSError', NativeError>
+export type KMSError = StashJSError<'KMSError', NativeError | PlainError >
 export const KMSError: (cause: unknown, message?: string) => KMSError = (cause, message) => ({ tag: 'KMSError', cause: wrap(cause), message })
 
-export type AWSFederationFailure = StashJSError<'AWSFederationFailure', NativeError>
+export type AWSFederationFailure = StashJSError<'AWSFederationFailure', NativeError | PlainError >
 export const AWSFederationFailure: (cause: unknown, message?: string) => AWSFederationFailure = (cause, message) => ({ tag: 'AWSFederationFailure', cause: wrap(cause), message })
 
-export type DecryptionFailure = StashJSError<'DecryptionFailure', NativeError>
-export const DecryptionFailure: (cause: unknown) => DecryptionFailure = (cause) => ({ tag: 'DecryptionFailure', cause: wrap(cause) })
+export type DecryptionFailure = StashJSError<'DecryptionFailure', NativeError | AuthenticationFailure>
+export const DecryptionFailure: (cause: DecryptionFailure["cause"]) => DecryptionFailure = (cause) => ({ tag: 'DecryptionFailure', cause })
 
-export type EncryptionFailure = StashJSError<'EncryptionFailure', NativeError>
+export type EncryptionFailure = StashJSError<'EncryptionFailure', NativeError | PlainError >
 export const EncryptionFailure: (cause: unknown) => EncryptionFailure = (cause) => ({ tag: 'EncryptionFailure', cause: wrap(cause) })
 
 export type StreamingPutFailure = StashJSError<'StreamingPutFailure', NativeError | GRPCError | AuthenticationFailure>
@@ -167,67 +180,83 @@ export const wrap
     } else if ((thrown as any)?.tag === 'NativeError') {
       return thrown as any as NativeError
     } else {
-      try {
-        return { tag: 'NativeError', cause: new Error(`${JSON.stringify(thrown)}`) }
-      } catch(error) {
-        return { tag: 'NativeError', cause: new Error(`${thrown}`) }
-      }
+      throw unreachable("If we get here then something is trying to wrap an error that is not an Error or NativeError")
     }
   }
 
   export function simpleDescriptionForError<Cause, E extends StashJSError<ErrorTag, Cause>>(error: E): string {
     switch (error.tag) {
-      case 'AnalysisFailure': return "Analysis of a record failed"
-      case 'AuthenticationFailure': return "Authentication failed"
-      case 'AWSFederationFailure': return "Failed to federate credentials to AWS"
-      case 'CollectionCreationFailure': return "Failed to create collection"
-      case 'CollectionDeleteFailure': return "Failed to delete collection"
-      case 'CollectionLoadFailure': return "Failed to load collection"
-      case 'CollectionListFailure': return "Failed to list collections"
-      case 'ConnectionFailure': return "Failed to connect to StashJS service"
-      case 'DecryptionFailure': return "Failed to decrypt"
-      case 'DeleteProfileFailure': return "Failed to delete profile config or auth token"
-      case 'DocumentDeleteFailure': return "Failed to delete document from a collection"
-      case 'DocumentGetFailure': return "Failed to get document from collection"
-      case 'DocumentGetAllFailure': return "Failed to get all documents from collection"
-      case 'DocumentPutFailure': return "Failed to put document in collection"
-      case 'DocumentQueryFailure': return "Failed to query documents"
-      case 'EncryptionFailure': return "Failed to encrypt"
-      case 'GRPCError': return "A gRPC error occurred"
-      case 'IllegalStateError': return "An internal error occurred: this is bug in StashJS"
-      case 'IOError': return "Error occurred reading or writing to the filesystem or the network"
-      case 'KMSError': return "KMS error"
-      case 'LoadProfileFailure': return "Failed to load Stash profile"
-      case 'LoadProfileNamesFailure': return "Failed to load Stash profile names"
-      case 'MalformedConfigFile': return "Stash profile was malformed"
-      case 'MissingConfigDir': return "Stash config directory not found"
-      case 'MissingProfile': return "Stash profile not found"
-      case 'NoDefaultProfileSet': return "No default Stash profile is set and StashJS was initiated without naming a specific profile"
-      case 'OAuthFailure': return "OAuth authentication failed"
-      case 'SaveProfileFailure': return "Failed to save profile"
-      case 'SetDefaultProfileFailure': return "Failed to set the default profile"
-      case 'StreamingPutFailure': return "Failure during streaming bulk upsert"
+      case 'AnalysisFailure': return "[AnalysisFailure] Analysis of a record failed"
+      case 'AuthenticationFailure': return "[AuthenticationFailure] Authentication failed"
+      case 'AWSFederationFailure': return "[AWSFederationFailure] Failed to federate credentials to AWS"
+      case 'CollectionCreationFailure': return "[CollectionCreationFailure] Failed to create collection"
+      case 'CollectionDeleteFailure': return "[CollectionDeleteFailure] Failed to delete collection"
+      case 'CollectionLoadFailure': return "[CollectionLoadFailure] Failed to load collection"
+      case 'CollectionListFailure': return "[CollectionListFailure] Failed to list collections"
+      case 'ConnectionFailure': return "[ConnectionFailure] Failed to connect to StashJS service"
+      case 'DecryptionFailure': return "[DecryptionFailure] Failed to decrypt"
+      case 'DeleteProfileFailure': return "[DeleteProfileFailure] Failed to delete profile config or auth token"
+      case 'DocumentDeleteFailure': return "[DocumentDeleteFailure] Failed to delete document from a collection"
+      case 'DocumentGetFailure': return "[DocumentGetFailure] Failed to get document from collection"
+      case 'DocumentGetAllFailure': return "[DocumentGetAllFailure] Failed to get all documents from collection"
+      case 'DocumentPutFailure': return "[DocumentPutFailure] Failed to put document in collection"
+      case 'DocumentQueryFailure': return "[DocumentQueryFailure] Failed to query documents"
+      case 'EncryptionFailure': return "[EncryptionFailure] Failed to encrypt"
+      case 'GRPCError': return "[GRPCError] GRPC Error"
+      case 'IllegalStateError': return "[IllegalStateError] An internal error occurred: this is bug in StashJS"
+      case 'IOError': return "[IOError] Error occurred reading or writing to the filesystem or the network"
+      case 'KMSError': return "[KMSError] KMS error"
+      case 'LoadProfileFailure': return "[LoadProfileFailure] Failed to load Stash profile"
+      case 'LoadProfileNamesFailure': return "[LoadProfileNamesFailure] Failed to load Stash profile names"
+      case 'MalformedConfigFile': return "[MalformedConfigFile] Stash profile was malformed"
+      case 'MissingConfigDir': return "[MissingConfigDir] Stash config directory not found"
+      case 'MissingProfile': return "[MissingProfile] Stash profile not found"
+      case 'NoDefaultProfileSet': return "[NoDefaultProfileSet] No default Stash profile is set and StashJS was initiated without naming a specific profile"
+      case 'OAuthFailure': return "[OAuthFailure] OAuth authentication failed"
+      case 'PlainError': return error.message
+      case 'NativeError': return `[JSError: ${(error.cause as any).name}]`
+      case 'SaveProfileFailure': return "[SaveProfileFailure] Failed to save profile"
+      case 'SetDefaultProfileFailure': return "[SetDefaultProfileFailure] Failed to set the default profile"
+      case 'StreamingPutFailure': return "[StreamingPutFailure] Failure during streaming bulk upsert"
     }
   }
 
-
-  export function toErrorMessage<Cause, E extends StashJSError<ErrorTag, Cause> | NativeError>(error: E): string {
-    let desc: string;
-    if (error.tag === 'NativeError') {
-      desc = `${ error.cause.message }`
-      if (error.cause.stack) {
-        desc = `(Native JS Error) ${ desc }\n${ error.cause.stack }`
-      }
+  function simpleDescriptionForErrorWithMessage<Cause, E extends StashJSError<ErrorTag, Cause>>(error: E): string {
+    const desc = simpleDescriptionForError(error)
+    if (error.tag === 'PlainError') {
       return desc
-    } else {
-      desc = simpleDescriptionForError(error)
+    } else if (error.tag !== 'NativeError') {
       if (error.message) {
-        desc = `${ desc } (${ error.message })`
+        return `${desc} (${error.message})`
       }
+    } else if (error.tag === 'NativeError') {
+      if (error.cause.message) {
+        return `${desc} (${error.cause.message})`
+      }
+    }
+    return desc
+  }
+
+
+  export function toErrorMessage<Cause, E extends StashJSError<ErrorTag, Cause>>(error: E, indentation: number = 0): string {
+    if (error.tag === 'PlainError') {
+      return withIndentation(indentation, simpleDescriptionForErrorWithMessage(error))
+    } if (error.tag === 'NativeError') {
+      return withIndentation(indentation, simpleDescriptionForErrorWithMessage(error)) +
+            "\n" + (error.cause.stack?.split(/\n/).map(line => withIndentation(indentation + 4, line)).join("\n"))
+    } else {
       if (error.cause) {
-        return `${ desc }\n${ toErrorMessage(error.cause as any)}`
+        return `${withIndentation(indentation, simpleDescriptionForErrorWithMessage(error))}\n${toErrorMessage((error.cause as any), indentation + 4)}`
       } else {
-        return desc
+        return withIndentation(indentation, simpleDescriptionForErrorWithMessage(error))
       }
+    }
+  }
+
+  function withIndentation(indentation: number, text: string): string {
+    if (indentation === 0) {
+      return text
+    } else {
+      return `${Array.from(Array(indentation - 3).keys()).map(_ => " ").join("")} └ ${text}`
     }
   }
