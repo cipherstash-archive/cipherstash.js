df65603bfcbe0ca27abb9a6df746af9deaf37f8c
Bennett Hardwick <bennett@cipherstash.com>
2022-03-30 16:06:04 +1000

    Write tsdoc for Collection

--- stashjs/src/collection.ts
+++ stashjs/src/collection.ts
@@ -16,18 +16,63 @@ export class Collection<
   public get name() { return this.collection.name }
   public get schema() { return this.collection.schema }
 
+  /**
+   * Retrieve a record by id
+   *
+   * ```ts
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection<Movie>('movies');
+   *
+   * const movie = await movies.get(...);
+   * ```
+   */
   public get(id: string | Buffer): Promise<R & HasID | null> {
     return convertPrivateApiResult(this.collection.get(id))
   }
 
+  /**
+   * Retrieve an array of records from an array of ids
+   *
+   * ```ts
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection<Movie>('movies');
+   *
+   * const [ first, second ] = await movies.getAll([ ..., ... ]);
+   * ```
+   */
   public getAll(ids: Array<string | Buffer>): Promise<Array<R>> {
     return convertPrivateApiResult(this.collection.getAll(ids))
   }
 
+  /**
+   * Upsert a record into the collection
+   *
+   * ```ts
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection<Movie>('movies');
+   *
+   * const id = await movies.put({
+   *   title: 'CipherStash Reloaded',
+   *   year: 2022
+   * });
+   * ```
+   */
   public put(doc: R): Promise<string> {
     return convertPrivateApiResult(this.collection.put(doc))
   }
 
+  /**
+   * Query a collection based on its schema
+   *
+   * ```ts
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection(movieSchema);
+   *
+   * const { documents } = await movies.query(
+   *   movie => movie.year.lte(1960)
+   * );
+   * ```
+   */
   public query(
     callbackOrQueryOptions: ((where: QueryBuilder<R, M>) => Query<R, M>) | QueryOptions<R, M>,
     queryOptions?: QueryOptions<R, M>
@@ -35,10 +80,38 @@ export class Collection<
     return convertPrivateApiResult(this.collection.query(callbackOrQueryOptions, queryOptions))
   }
 
+  /**
+   * Delete a record by id
+   *
+   * ```ts
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection<Movie>('movies');
+   *
+   * const movie = await movies.delete(...);
+   * ```
+   */
   public delete(id: string | Buffer): Promise<null> {
     return convertPrivateApiResult(this.collection.delete(id))
   }
 
+  /**
+   * Put multiple records into a collection using an async generator
+   *
+   * ```ts
+   * async function* fetchMovies() {
+   *   const first = await fetchFirstMovie();
+   *   yield first;
+   *
+   *   const second = await fetchSecondMovie();
+   *   yield second;
+   * }
+   *
+   * const stash = await Stash.connect();
+   * const movies = await stash.loadCollection<Movie>('movies');
+   *
+   * awat movies.putStream(fetchMovies);
+   * ```
+   */
   public putStream(records: AsyncIterator<R>): Promise<V1.Document.StreamingPutReply> {
     return convertPrivateApiResult(this.collection.putStream(records))
   }
