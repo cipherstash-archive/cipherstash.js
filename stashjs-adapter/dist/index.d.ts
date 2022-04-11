16a041bbca682ac0a00a94207ebd86c94f5db081
Dan Draper <dan@coderdan.co>
2022-04-11 20:23:48 +1000

    Changed approach by using an external mapper for more flexibility

--- stashjs-adapter/dist/index.d.ts
+++ stashjs-adapter/dist/index.d.ts
@@ -1,20 +1,31 @@
-import { Collection, Mappings, MappingsMeta } from "@cipherstash/stashjs";
+import { Mappings, QueryBuilder, Query, QueryOptions } from "@cipherstash/stashjs";
 export declare type CSType<T> = Omit<T, "id"> & {
+    id?: string;
     originalId: number;
-    id: string;
 };
 declare type MappingsWrapper<T> = Mappings<CSType<T>>;
-declare type MappingsMetaWrapper<T> = MappingsMeta<MappingsWrapper<T>>;
-declare type CollectionWrapper<T> = Collection<CSType<T>, MappingsWrapper<T>, MappingsMetaWrapper<T>>;
+export declare type QueryCallback<T> = (where: QueryBuilder<CSType<T>, MappingsWrapper<T>>) => Query<CSType<T>, MappingsWrapper<T>>;
+export declare type CollectionQueryOptions<T> = QueryOptions<CSType<T>, MappingsWrapper<T>>;
+export declare type QueryCallbackOrOptions<T> = QueryCallback<T> | CollectionQueryOptions<T>;
+export interface RecordMapper {
+    setStashId: (record: {
+        id: number;
+    }, stashId: string | null) => void;
+    findStashIdsFor: (id: Array<number>) => Promise<Array<string>>;
+    newIdFor: (stashId: string) => Promise<number>;
+}
 export declare class CollectionAPI<T extends {
     id: number;
+    stashId?: string | null;
 }> {
-    collection: Promise<CollectionWrapper<T>>;
-    idNamespace: string;
-    constructor(name: string, idNamespace: string);
-    put(record: T): Promise<string>;
-    get(id: number): Promise<T>;
-    query: CollectionWrapper<T>["query"];
-    list(): Promise<Array<T>>;
+    #private;
+    constructor(name: string, plaintextStore: RecordMapper);
+    create(record: Omit<T, "id" | "stashId">): Promise<T>;
+    put(record: T): Promise<T>;
+    get(id: number): Promise<T | null>;
+    getAll(ids: Array<number>): Promise<Array<T>>;
+    delete(id: number): Promise<void>;
+    query(callbackOrQueryOptions: QueryCallbackOrOptions<T>, queryOptions?: CollectionQueryOptions<T>): Promise<Array<T>>;
+    list(options?: CollectionQueryOptions<T>): Promise<Array<T>>;
 }
 export {};
