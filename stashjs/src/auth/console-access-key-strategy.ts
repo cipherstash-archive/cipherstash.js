4434305737a9ca518f1e8c85d39588550068952e
fimac <fiona@cipherstash.com>
2022-05-09 09:14:08 +1000

    format files

--- stashjs/src/auth/console-access-key-strategy.ts
+++ stashjs/src/auth/console-access-key-strategy.ts
@@ -1,62 +1,55 @@
-import { OauthAuthenticationInfo, stashOauth } from "./oauth-utils";
-import { AuthStrategy } from "./auth-strategy";
-import { ConsoleAccessKey, StashConfiguration } from "../stash-config";
-import { AuthenticationFailure } from "../errors";
-import { AsyncResult, Err, Ok } from "../result";
+import { OauthAuthenticationInfo, stashOauth } from "./oauth-utils"
+import { AuthStrategy } from "./auth-strategy"
+import { ConsoleAccessKey, StashConfiguration } from "../stash-config"
+import { AuthenticationFailure } from "../errors"
+import { AsyncResult, Err, Ok } from "../result"
 
 export type StashProfileConsoleAccessKey = {
   config: Omit<StashConfiguration, "identityProvider"> & {
-    identityProvider: ConsoleAccessKey;
-  };
-};
+    identityProvider: ConsoleAccessKey
+  }
+}
 
-export class ConsoleAccessKeyStrategy
-  implements AuthStrategy<OauthAuthenticationInfo>
-{
+export class ConsoleAccessKeyStrategy implements AuthStrategy<OauthAuthenticationInfo> {
   private oauthCreds: OauthAuthenticationInfo = {
     accessToken: "",
     refreshToken: "",
     expiry: 0,
-  };
+  }
 
   constructor(private profile: StashProfileConsoleAccessKey) {}
 
   public stillFresh(): boolean {
-    return !this.needsRefresh();
+    return !this.needsRefresh()
   }
 
-  public async getAuthenticationDetails(): AsyncResult<
-    OauthAuthenticationInfo,
-    AuthenticationFailure
-  > {
+  public async getAuthenticationDetails(): AsyncResult<OauthAuthenticationInfo, AuthenticationFailure> {
     if (this.needsRefresh()) {
-      const tokenResult = await this.acquireAccessToken();
+      const tokenResult = await this.acquireAccessToken()
       if (!tokenResult.ok) {
-        return Err(tokenResult.error);
+        return Err(tokenResult.error)
       }
     }
 
-    return Ok(this.oauthCreds);
+    return Ok(this.oauthCreds)
   }
 
   private needsRefresh(): boolean {
-    return Date.now() / 1000 - EXPIRY_BUFFER_SECONDS > this.oauthCreds.expiry;
+    return Date.now() / 1000 - EXPIRY_BUFFER_SECONDS > this.oauthCreds.expiry
   }
 
   private async acquireAccessToken(): AsyncResult<void, AuthenticationFailure> {
-    const oauthInfo = await stashOauth.authenticateViaConsoleAccessKey(
-      this.profile.config.identityProvider.accessKey,
-    );
+    const oauthInfo = await stashOauth.authenticateViaConsoleAccessKey(this.profile.config.identityProvider.accessKey)
 
     if (oauthInfo.ok) {
-      this.oauthCreds = oauthInfo.value;
-      return Ok();
+      this.oauthCreds = oauthInfo.value
+      return Ok()
     } else {
-      return Err(oauthInfo.error);
+      return Err(oauthInfo.error)
     }
   }
 }
 
 /* Refresh tokens before the expiry to avoid API errors due
  * to race conditions. Expiry buffer is in seconds */
-const EXPIRY_BUFFER_SECONDS = 20;
+const EXPIRY_BUFFER_SECONDS = 20
