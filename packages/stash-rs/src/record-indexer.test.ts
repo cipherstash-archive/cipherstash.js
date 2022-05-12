9e41daee699e1ba08a11f6c0580edfb4a2248b55
Bennett Hardwick <bennett@cipherstash.com>
2022-05-12 15:12:06 +1000

    Add indexer, change to

---
+++ packages/stash-rs/src/record-indexer.test.ts
@@ -0,0 +1,141 @@
+import { RecordIndexer } from "./record-indexer"
+
+const prf_key = Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+const prp_key = Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+const index_id = Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+const record_id = Buffer.from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+describe("RecordIndexer", () => {
+  describe("init", () => {
+    test("load from schema", () => {
+      expect(() =>
+        RecordIndexer.init({
+          type: {
+            title: "string",
+            runningTime: "uint64",
+          },
+          indexes: {
+            exactTitle: {
+              mapping: { kind: "exact", field: "title" },
+              index_id,
+              prf_key,
+              prp_key,
+            },
+          },
+        })
+      ).not.toThrow()
+    })
+
+    test("invalid key in schema", () => {
+      expect(() =>
+        RecordIndexer.init({
+          type: {
+            title: "string",
+            runningTime: "uint64",
+          },
+          indexes: {
+            exactTitle: {
+              mapping: { kind: "exact", field: "title" },
+              index_id,
+              prf_key: Buffer.from([1, 2]),
+              prp_key,
+            },
+          },
+        })
+      ).toThrow(/invalid length 2/)
+    })
+
+    test("invalid index_id in schema", () => {
+      expect(() =>
+        RecordIndexer.init({
+          type: {
+            title: "string",
+            runningTime: "uint64",
+          },
+          indexes: {
+            exactTitle: {
+              mapping: { kind: "exact", field: "title" },
+              index_id: Buffer.from([1, 2, 3, 4]),
+              prf_key,
+              prp_key,
+            },
+          },
+        })
+      ).toThrow(/invalid length 4/)
+    })
+  })
+
+  test("index record", () => {
+    const indexer = RecordIndexer.init({
+      type: {
+        title: "string",
+        runningTime: "uint64",
+      },
+      indexes: {
+        exactTitle: {
+          mapping: { kind: "exact", field: "title" },
+          index_id,
+          prf_key,
+          prp_key,
+        },
+      },
+    })
+
+    const vectors = indexer.encryptRecord({
+      id: record_id,
+      title: "What a great title!",
+    })
+
+    expect(vectors).toHaveLength(1)
+  })
+
+  test("index record empty record", () => {
+    const indexer = RecordIndexer.init({
+      type: {
+        title: "string",
+        runningTime: "uint64",
+      },
+      indexes: {
+        exactTitle: {
+          mapping: { kind: "exact", field: "title" },
+          index_id,
+          prf_key,
+          prp_key,
+        },
+      },
+    })
+
+    const vectors = indexer.encryptRecord({
+      id: record_id,
+    })
+
+    expect(vectors).toHaveLength(0)
+  })
+
+  test("index record with Uint8Array id", () => {
+    const indexer = RecordIndexer.init({
+      type: {
+        title: "string",
+        runningTime: "uint64",
+      },
+      indexes: {
+        exactTitle: {
+          mapping: { kind: "exact", field: "title" },
+          index_id,
+          prf_key,
+          prp_key,
+        },
+      },
+    })
+
+    const vectors = indexer.encryptRecord({
+      id: new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]),
+      title: "Great title!",
+    });
+
+    expect(vectors).toHaveLength(1)
+  })
+})
