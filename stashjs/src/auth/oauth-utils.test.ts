cf747fef11d5022018b24b42f4035811ebf926bf
fimac <fiona@cipherstash.com>
2022-05-19 19:06:36 +1000

    add isExpired token helper fn and test

---
+++ stashjs/src/auth/oauth-utils.test.ts
@@ -0,0 +1,21 @@
+import { isExpired } from "./oauth-utils"
+
+describe("isExpired", () => {
+  test("When now + buffer is less than expiry return false", () => {
+    const now = 1652900000
+    const buffer = 120
+    const expiry = now + buffer + 1
+
+    const result = isExpired(buffer, expiry, now)
+    expect(result).toEqual(false)
+  })
+
+  test("When now + buffer is greater or equal to expiry returns true", () => {
+    const now = 1652900000
+    const buffer = 120
+    const expiry = now + buffer
+
+    const result = isExpired(buffer, expiry, now)
+    expect(result).toEqual(true)
+  })
+})
