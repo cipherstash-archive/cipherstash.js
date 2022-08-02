8461d32dbab2811a75b0f9babaae54b75f4933c8
Dan Draper <dan@cipherstash.com>
2022-08-02 20:05:03 +1000

    Orderable queries implemented

--- stashjs-typeorm/src/indexing-subscriber.ts
+++ stashjs-typeorm/src/indexing-subscriber.ts
@@ -9,7 +9,7 @@ import {
   Logger,
 } from "typeorm"
 import { deleteEntity, ensureStashID, mapAndPutEntity } from "./collection-adapter"
-import { isStashed, StashLinkable, StashLinked } from "./types"
+import { isStashed, StashLinkableEntity, StashLinkedEntity } from "./types"
 
 type StashSyncEvent = "insert" | "update" | "remove"
 
@@ -21,36 +21,36 @@ function logStashEvent(logger: Logger, event: StashSyncEvent, stashId: string): 
 @EventSubscriber()
 export class IndexingSubscriber implements EntitySubscriberInterface {
   /* Ensure stashID is set on insertion */
-  beforeInsert({ entity }: InsertEvent<StashLinkable>): void {
+  beforeInsert({ entity }: InsertEvent<StashLinkableEntity>): void {
     ensureStashID(entity)
   }
 
   /* Ensure stashID is set on recovered records */
-  beforeRecover({ entity }: RecoverEvent<StashLinkable>): void {
+  beforeRecover({ entity }: RecoverEvent<StashLinkableEntity>): void {
     ensureStashID(entity)
   }
 
   /* Ensure stashID is set on update */
-  beforeUpdate({ entity }: UpdateEvent<StashLinkable>): void {
-    ensureStashID(entity as StashLinkable)
+  beforeUpdate({ entity }: UpdateEvent<StashLinkableEntity>): void {
+    ensureStashID(entity as StashLinkableEntity)
   }
 
   /* Sync to collection after insert */
-  async afterInsert({ connection, entity, metadata }: InsertEvent<StashLinked>): Promise<string> {
+  async afterInsert({ connection, entity, metadata }: InsertEvent<StashLinkedEntity>): Promise<string> {
     return await mapAndPutEntity(entity, metadata.tablePath).then(stashId =>
       logStashEvent(connection.logger, "insert", stashId)
     )
   }
 
   /* Sync to collection after update */
-  async afterUpdate({ connection, entity, metadata }: UpdateEvent<StashLinked>): Promise<string> {
-    return await mapAndPutEntity(entity as StashLinked, metadata.tablePath).then(stashId =>
+  async afterUpdate({ connection, entity, metadata }: UpdateEvent<StashLinkedEntity>): Promise<string> {
+    return await mapAndPutEntity(entity as StashLinkedEntity, metadata.tablePath).then(stashId =>
       logStashEvent(connection.logger, "update", stashId)
     )
   }
 
   /* Remove from collection after DB removal */
-  async afterRemove({ databaseEntity, connection, entity, metadata }: RemoveEvent<StashLinkable>): Promise<string> {
+  async afterRemove({ databaseEntity, connection, metadata }: RemoveEvent<StashLinkableEntity>): Promise<string> {
     if (isStashed(databaseEntity)) {
       return await deleteEntity(databaseEntity, metadata.tableName).then(stashId =>
         logStashEvent(connection.logger, "remove", stashId)
@@ -62,9 +62,8 @@ export class IndexingSubscriber implements EntitySubscriberInterface {
   async afterSoftRemove({
     databaseEntity,
     connection,
-    entity,
     metadata,
-  }: SoftRemoveEvent<StashLinkable>): Promise<string> {
+  }: SoftRemoveEvent<StashLinkableEntity>): Promise<string> {
     if (isStashed(databaseEntity)) {
       return await deleteEntity(databaseEntity, metadata.tableName).then(stashId =>
         logStashEvent(connection.logger, "remove", stashId)
@@ -73,7 +72,7 @@ export class IndexingSubscriber implements EntitySubscriberInterface {
   }
 
   /* Sync to collection after recovery */
-  async afterRecover({ connection, entity, metadata }: RecoverEvent<StashLinked>): Promise<string> {
+  async afterRecover({ connection, entity, metadata }: RecoverEvent<StashLinkedEntity>): Promise<string> {
     return await mapAndPutEntity(entity, metadata.tablePath).then(stashId =>
       logStashEvent(connection.logger, "update", stashId)
     )
