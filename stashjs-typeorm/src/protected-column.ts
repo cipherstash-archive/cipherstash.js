ff584461cfb8dba4c35f9d979eb2ba6da4677d56
Dan Draper <dan@cipherstash.com>
2022-08-02 22:59:34 +1000

    Add typeorm-encrypted and include in ProtectedColumn decorator

--- stashjs-typeorm/src/protected-column.ts
+++ stashjs-typeorm/src/protected-column.ts
@@ -1,4 +1,5 @@
 import { Column, ColumnOptions } from "typeorm"
+import { EncryptionTransformer } from "typeorm-encrypted"
 
 export interface IndexOptions {
   kind?: string
@@ -9,7 +10,15 @@ export interface ProtectedColumnOptions extends ColumnOptions {
 }
 
 export const ProtectedColumn = (options?: ProtectedColumnOptions) => {
-  return Column({ index: {}, ...options })
+  return Column({
+    index: {},
+    transformer: new EncryptionTransformer({
+      key: "e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61", // TODO: Don't hardcode key
+      algorithm: "aes-256-gcm",
+      ivLength: 16,
+    }),
+    ...options,
+  })
 }
 
 // TODO: ProtectedColumn or EncryptedColumn probably has to wrap the Type ORM Column decorator
