25e527faac7afa7ef8eb191e8ea722b87f33fc64
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-08-04 15:07:50 +0800

    stashjs-grpc build scripts

--- packages/stashjs-grpc/scripts/gen-protos.sh
+++ packages/stashjs-grpc/scripts/gen-protos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script builds everything in the platform repo. It is written using the
+# This script generate the protos binaries from the proto files. It is written using the
 # bash script best practices (see: https://kvz.io/bash-best-practices.html)
 
 set -e # exit when a command fails
@@ -9,13 +9,11 @@ set -x # trace what gets executed (useful for debugging)
 
 trap "echo SOMETHING WENT WRONG - please read the logs above and see if it helps you figure out what is wrong - and also ask an engineer help" ERR
 
-rm -fr ./dist ./generated
-mkdir -p ./dist ./generated/stash/GRPC
+rm -fr ./generated
+mkdir -p ./generated/stash/GRPC
 
 proto-loader-gen-types api.proto --outDir=generated --grpcLib=@grpc/grpc-js \
   --includeDirs grpc/v1 --keepCase=true --longs=number --enums=string \
   --defaults=false --oneofs==true
 
 ./scripts/pack-api.sh
-npx -p typescript tsc
-cp -RL grpc dist/generated/stash/GRPC
