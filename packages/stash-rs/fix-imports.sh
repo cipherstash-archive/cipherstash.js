8b54dc1c2fff06d49330d6e1f16f0ca854c8d993
Bennett Hardwick <bennett@cipherstash.com>
2022-10-25 15:52:54 +1000

    Convert stash-rs to use wasm
    Converts @cipherstash/stash-rs to use wasm instead of neon and native
    code.
    
    Also updates stashjs types, bumps stash-cli, stashjs and stash-rs
    versions, and fixes some eager packages.

---
+++ packages/stash-rs/fix-imports.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Update the snippet to use Node.js compatible JS.
+sed -i 's/export function bytes_literal() { return "bytes"; }/module.exports = { bytes_literal() { return "bytes"; } }/g' ./pkg/snippets/wasm-streams-42e57edbcd526312/inline0.js
