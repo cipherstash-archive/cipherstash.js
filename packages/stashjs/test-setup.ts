a9756986651fdc0da86c207c60653267e17906a3
Bennett Hardwick <bennett@cipherstash.com>
2022-10-27 14:38:44 +1000

    Fix stash-js tests

--- packages/stashjs/test-setup.ts
+++ packages/stashjs/test-setup.ts
@@ -1,8 +1,11 @@
 import { vol } from "memfs"
+import "@cipherstash/stash-rs"
+
+// Since stash-rs needs to use the filesytem to load the wasm module it needs to be loaded before this mock is set.
+// To prevent this mock from getting hoisted use eval
+eval('jest.mock("fs", () => require("memfs").fs)')
 
 Error.stackTraceLimit = 1024
-
-jest.mock("fs", () => require("memfs").fs)
 
 afterEach(() => {
   vol.reset()
